2de94e10394f87c1d85ee222671e67ad48790903
Heather <campbell.hf@gmail.com>
2016-03-08 12:13:14 +0000

    Created a page with variable number of counters.

--- app/js/controllers/countersController.js
+++ app/js/controllers/countersController.js
@@ -6,4 +6,25 @@ countersApp.controller("countersCtrl", function(){
   self.increment = function() {
     self.count++;
   }
+
+  self.decrement = function() {
+      self.count--;
+  }
+});
+
+countersApp.controller("countersNumber", function(){
+  var self = this;
+
+  self.number = 5
+
+  self.getNumber = function() {
+    var x = [];
+    for(var i=0; i<self.number; i++) {
+      x.push(i);
+    }
+    return x;
+  };
+
+
+
 });
